59186cc69236e4b60774ca84f2c1c0a0b4a51bbd
Jefferson Chaves <jefferson.chaves@ifpr.edu.br>
2025-05-26 22:20:14 -0300

    feat: add insert

--- src/main/java/com/example/camada_persistencia_aula/repositories/SellerRepository.java
+++ src/main/java/com/example/camada_persistencia_aula/repositories/SellerRepository.java
@@ -1,129 +1,15 @@
 package com.example.camada_persistencia_aula.repositories;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import com.example.camada_persistencia_aula.connection.ConnectionFactory;
-import com.example.camada_persistencia_aula.exceptions.DatabaseException;
-import com.example.camada_persistencia_aula.models.Department;
 import com.example.camada_persistencia_aula.models.Seller;
 
-public class SellerRepository {
-
-    private Connection connection;
-
-    public SellerRepository() {
-        connection = ConnectionFactory.getConnection();
-    }
-
-    public List<Seller> getAll() {
-
-        List<Seller> sellers = new ArrayList<>();
-
-        Statement statement = null;
-        ResultSet result = null;
-
-        try {
-            statement = connection.createStatement();
-
-            result = statement.executeQuery("select seller.*, department.Name as DepartmentName " +
-                    "from seller " +
-                    "join department " +
-                    "on seller.DepartmentId = department.Id");
-
-            Map<Integer, Department> departmentMap = new HashMap<>();
-
-            while (result.next()) {
-
-                Department department = departmentMap.get(result.getInt("DepartmentId"));
-
-                if (department == null) {
-                    department = instantiateDepartment(result);
-                    departmentMap.put(result.getInt("DepartmentId"), department);
-                }
-
-                Seller seller = instantiateSeller(result, department);
-
-                sellers.add(seller);
-            }
-
-        } catch (SQLException e) {
-            throw new DatabaseException("não foi possível criar um Statment");
-        } finally {
-            ConnectionFactory.closeSResultSet(result);
-            ConnectionFactory.closeStatament(statement);
-        }
-
-        return sellers;
-
-    }
-
-    public List<Seller> getSellersByDepartmentId(Integer departmentId){
-
-        List<Seller> sellers = new ArrayList<>();
-        PreparedStatement statement = null;
-        ResultSet result = null;
-
-        try {
-            statement = connection.prepareStatement("select seller.*, department.Name as DepartmentName " +
-                    "from seller " +
-                    "join department " +
-                    "on seller.DepartmentId = department.Id " +
-                    "where department.id = ?");
-
-            statement.setInt(1, departmentId);
-            result = statement.executeQuery();
-            
-            Map<Integer, Department> mapDepartments = new HashMap<>();
-
-            while (result.next()) {
-
-                Department department = mapDepartments.get(result.getInt("DepartmentId"));
-
-                if (department == null) {
-                    department = instantiateDepartment(result);
-                    mapDepartments.put(result.getInt("DepartmentId"), department);
-                }
-
-                Seller seller = instantiateSeller(result, department);
-            }
-            
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-
-    }
-
-
-    public Department instantiateDepartment(ResultSet result) throws SQLException {
-
-        Department department = new Department();
-        department.setId(result.getInt("DepartmentId"));
-        department.setName(result.getString("DepartmentName"));
-
-        return department;
-    }
-
-    public Seller instantiateSeller(ResultSet result, Department department) throws SQLException {
-        
-        Seller seller = new Seller();
-
-        seller.setId(result.getInt("Id"));
-        seller.setName(result.getString("Name"));
-        seller.setEmail(result.getString("Email"));
-        seller.setBirthDate(result.getDate("BirthDate").toLocalDate());
-        seller.setBaseSalary(result.getDouble("BaseSalary"));
-        seller.setDepartment(department);
-
-        return seller;
-
-    }
+public interface SellerRepository {
+    
+    public List<Seller> getAll();
+    public List<Seller> getSellersByDepartmentId(Integer departmentId);
+    public Seller insert(Seller seller);
+    public Seller update(Seller seller);
+    public void delete(Integer id);
 
 }
--- src/test/java/com/example/camada_persistencia_aula/connection/ConnectionFactoryTeste.java
+++ src/test/java/com/example/camada_persistencia_aula/connection/ConnectionFactoryTeste.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -11,8 +12,10 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.camada_persistencia_aula.models.Department;
 import com.example.camada_persistencia_aula.models.Seller;
 import com.example.camada_persistencia_aula.repositories.SellerRepository;
+import com.example.camada_persistencia_aula.repositories.SellerRepositoryJDBC;
 
 @SpringBootTest
 public class ConnectionFactoryTeste {
@@ -45,7 +48,7 @@ public class ConnectionFactoryTeste {
     @Test
     public void deveRetornarUmaListaDeSellers(){
 
-        SellerRepository repository = new SellerRepository();
+        SellerRepositoryJDBC repository = new SellerRepositoryJDBC();
 
         List<Seller> sellers = repository.getAll();
 
@@ -56,5 +59,40 @@ public class ConnectionFactoryTeste {
         assertThat(sellers.size()).isGreaterThan(0);
 
     }
+
+    @Test
+    public void deveRetornarUmaListaDeSellersPorDepartamento(){
+
+        SellerRepository repository = new SellerRepositoryJDBC();
+
+        List<Seller> sellers = repository.getSellersByDepartmentId(2);
+
+        for (Seller seller : sellers) {
+            System.out.println(seller);
+        }
+
+        assertThat(sellers.size()).isGreaterThan(0);
+
+    }
+
+    @Test
+    public void deveInserirUmSellerComSucesso(){
+        SellerRepository repository = new SellerRepositoryJDBC();
+
+        Department department = new Department();
+        department.setId(1);
+
+        Seller s1 = new Seller();
+        s1.setName("Carl");
+        s1.setEmail("carl@gmail.com");
+        s1.setBirthDate(LocalDate.of(1985, 01, 01));
+        s1.setBaseSalary(1000.0);
+        s1.setDepartment(department);
+
+        Seller result = repository.insert(s1);
+
+        assertThat(result).isNotNull();
+    }
+
     
 }
